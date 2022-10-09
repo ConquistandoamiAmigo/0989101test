4e043214187bb5eb2899eef060965ed5a0c8353c
ConquistandoamiAmigo <vadimnoluv@gmail.com>
2022-10-10 01:45:38 +0300

    Update post-header.tsx

--- components/post-header.tsx
+++ components/post-header.tsx
@@ -1,4 +1,5 @@
 import Avatar from './avatar'
+import DateFormatter from './date-formatter'
 import CoverImage from './cover-image'
 import PostTitle from './post-title'
 import type Author from '../interfaces/author'
@@ -6,6 +7,7 @@ import type Author from '../interfaces/author'
 type Props = {
   title: string
   coverImage: string
+  date: string
   author: Author
 }
 
@@ -22,6 +24,9 @@ const PostHeader = ({ title, coverImage, date, author }: Props) => {
       <div className="max-w-2xl mx-auto">
         <div className="block md:hidden mb-6">
           <Avatar name={author.name} picture={author.picture} />
+        </div>
+        <div className="mb-6 text-lg">
+          <DateFormatter dateString={date} />
         </div>
       </div>
     </>
